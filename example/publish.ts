42e43f9230dedd6f9e7e06ee91a0ab217d56289c
eugene antonevich <eugeneantonevich@eugenes-Mac-mini.local>
2018-03-13 17:46:56 +0300

    .

--- example/publish.ts
+++ example/publish.ts
@@ -10,11 +10,11 @@ import "isomorphic-fetch";
     setup process environment
  */
 
-process.env.AWS_IOT_ENDPOINT = "a1iocoaxmt1h7b.iot.us-east-1.amazonaws.com";
-process.env.AWS_REGION = "us-east-1";
-process.env.AWS_USER_POOL_ID = "us-east-1_LrSYtlfnH";
-process.env.AWS_CLIENT_ID = "1lr8bpt6bbru48ou760ufcn2tt";
-process.env.AWS_IDENTITY_POOL_ID = "us-east-1:7eb56e23-0840-4ffe-ab40-d0a2b5006de4";
+process.env.AWS_IOT_ENDPOINT = "";
+process.env.AWS_REGION = "";
+process.env.AWS_USER_POOL_ID = "";
+process.env.AWS_CLIENT_ID = "";
+process.env.AWS_IDENTITY_POOL_ID = "";
 process.env.DEBUG_MQTT_CLIENT = "true";
 
 if (process.argv.length < 4) {
